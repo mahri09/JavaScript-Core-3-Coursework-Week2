d3acd19d96158cea1bbc0318881e68d3097f212e
mahri09 <63245237+mahri09@users.noreply.github.com>
2022-03-05 11:12:06 +0000

    changed structure in dog photo app

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -1,13 +1,20 @@
 const submitBtn = document.getElementById("submit");
-const image = document.querySelector("img");
+const ulEl = document.getElementById("imageLine");
 
+const addImage = (data) => {
+  let liEl = document.createElement("li");
+  let image = document.createElement("img");
+  ulEl.appendChild(liEl);
+  liEl.append(image);
+  image.setAttribute("src", data);
+};
 submitBtn.addEventListener("click", () => {
   fetch("https://dog.ceo/api/breeds/image/random")
     .then((response) => {
       return response.json();
     })
-    .then((json) => {
-      image.setAttribute("src", json.message);
+    .then((data) => {
+      addImage(data.message);
     })
     .catch((error) => {
       console.log(error);
